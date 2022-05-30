63545d7a3c42dae77482b6c135137e4b9971ed5d
Sai Chaitanya Aluru <alurusaichaitanya@gmail.com>
2022-05-30 19:42:19 +0800

    feat: click ball feature added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: MyApp(),
       ),
     );
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('8 Ask'),
+          backgroundColor: Colors.blue,
+        ),
+        backgroundColor: Colors.lightBlue,
+        body: BallPage());
+  }
+}
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  var BallCount = 1;
+
+  void changeBall() {
+    setState(() {
+      BallCount = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: TextButton(
+            onPressed: () {
+              changeBall();
+            },
+            child: Image.asset('images/ball$BallCount.png')),
+      ),
+    );
+  }
+}
